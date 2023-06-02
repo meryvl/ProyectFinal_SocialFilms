6ed37878d3d9d091f4ad0efe7087a4d744a663b4
meryvl <meryvalerolaguna@gmail.com>
2023-06-02 10:35:02 +0000

    new commit addSee

--- src/front/js/component/AddSee/AddSee.js
+++ src/front/js/component/AddSee/AddSee.js
@@ -5,14 +5,22 @@ import { getSee } from "../../store/Fetch/Fetch";
 export const AddSee =(id)=>{
     
     const [see , setSee]=useState([])
+    const [classAdd ,setClass]=("addSee")
    
     const hanledSee=(id)=>{
         setSee(id)
         console.log(see)
+        
         }
+console.log(see)
 return(
     <>
-    <button className="addSee" onClick={()=>{hanledSee(id)}}>Add see</button>
+    {see.length < 1 ?
+ <button className="addSee" onClick={()=>{hanledSee(id)}}>Add see</button>
+ :
+ <img className="ok" src="https://static.vecteezy.com/system/resources/previews/002/736/131/original/check-ok-symbol-isolated-icon-free-vector.jpg"/>  
+}
+   
     </>
 )
 }
